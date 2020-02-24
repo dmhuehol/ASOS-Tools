48e2ba98cd143c7bdefb3154290a0d4b77d8b830
Daniel Hueholt <dmhuehol@ncsu.edu>
2020-02-24 15:13:40 -0500

    Add datenum output to weatherCodeSearch
    weatherCodeSearch now outputs the exact datenums, as well as the datestrings. Datenums are at times easier to index. Additionally, makes minor readability changes to the documentation.

--- weatherCodeSearch.m
+++ weatherCodeSearch.m
@@ -1,43 +1,47 @@
 %%weatherCodeSearch
-    %Function to find all dates and times when a given weather code
-    %occurred in a given ASOS data structure.
+    %Function to find dates and times that an input weather code occurred
+    %in an ASOS data structure.
     %
-    %General form: [dates,exactTimes] = weatherCodeSearch(weatherCode,ASOS)
+    %General form: [dates,exactTimes,exactDatenums] = weatherCodeSearch(weatherCode,ASOS)
     %
     %Outputs:
-    %dates: all dates where the input code was observed.
+    %dates: all dates where the input code was observed
     %exactTimes: the exact dates and times where the input code was observed
-    %   in DMY HMS format (all second entries are fake zeros).
+    %   in DMY HMS format (note that seconds are placeholder zeros)
+    %exactDatenums: same as exactTimes, but as MATLAB datenums (note that
+    %seconds are placeholder zeros)
     %
     %Inputs:
-    %weatherCode: an ASOS weather code, which must be input as a string.
-    %   Common codes: 'RA' = rain, 'SN' = snow, 'PL' = sleet, 'FZRA' =
+    %weatherCode: an ASOS weather code string
+    %   Precip codes: 'RA' = rain, 'SN' = snow, 'PL' = sleet, 'FZRA' =
     %   freezing rain, 'FZDZ' = freezing drizzle, 'DZ' = drizzle
+    %   Obscuration codes: 'BR' = mist, 'FG' = fog, 'FU' = smoke, 'HZ' =
+    %   haze
+    %   Character codes: 'SQ' = squall
+    %   
     %ASOS: an ASOS 5-minute data structure.
     %
     %Written by: Daniel Hueholt
     %North Carolina State University
     %Undergraduate Research Assistant at Environment Analytics
-    %Version Date: 6/05/2018
-    %Last Major Revision: 6/05/2018
-    %
-    %See also ASOSimportFiveMin
+    %Version Date: 2/24/2020
+    %Last Major Revision: 2/24/2020
     %
 
-function [dates,exactTimes] = weatherCodeSearch(weatherCode,ASOS)
+function [dates,exactTimes,exactDatenums] = weatherCodeSearch(weatherCode,ASOS)
 presentWeather = {ASOS.PresentWeather};
 
 weather = strfind(presentWeather,weatherCode);
 noWeather = cellfun('isempty',weather); %'isempty' is faster than @isempty
-weather(noWeather) = {0}; %Insert 0 into all empty cells, otherwise conversion to double will remove all blank entries
-weather = cell2mat(weather); %Make double
+weather(noWeather) = {0}; %Insert 0 into all empty cells, otherwise conversion to double removes blank entries
+weather = cell2mat(weather); %Convert to double
 logicalWeather = logical(weather); %Logically index on ~0
 %All logical 1 elements now correspond to where the weather code is detected
 validInd = logicalWeather==1;
 
 %Useful command window message if no weather codes were found
 if isempty(nonzeros(validInd))==1
-    dates = []; exactTimes = []; %Set outputs
+    dates = []; exactTimes = []; %Null outputs
     msg = 'No instances of this weather code could be located.';
     disp(msg)
     return %End the function
@@ -54,6 +58,6 @@ fakeSecond = zeros(length(validMinute),1); %Assume all seconds are zero entries
 validDates = datenum(validYear,validMonth,validDay);
 validDates = unique(validDates);
 dates = datestr(validDates); %Output unique days where code occurred
-validExactTimes = datenum(validYear,validMonth,validDay,validHour,validMinute,fakeSecond);
-exactTimes = datestr(validExactTimes); %Output every time where the code occurred
+exactDatenums = datenum(validYear,validMonth,validDay,validHour,validMinute,fakeSecond);
+exactTimes = datestr(exactDatenums); %Output every time where the code occurred
 end
