e47c4a6d6918ae75051aad197ea5c84e937211b2
Daniel Hueholt <dmhuehol@ncsu.edu>
2020-02-24 16:01:00 -0500

    Adds ability to download multiple stations at once
    ASOSdownloadFiveMin now allows a cell array of input stations. The output cell array of filenames is now 3-dimensional, with D1 corresponding to the year, D2 to the station, and D3 to the month.

--- ASOSdownloadFiveMin.m
+++ ASOSdownloadFiveMin.m
@@ -7,8 +7,7 @@
     %Inputs:
     %emailAddress: The user's complete email address. This is required by
     %the NCDC FTP server.
-    %station: Four character station ID, such as KHWV for Brookhaven Airport,
-    %or KISP for Islip.
+    %station: Four character station ID or cell array of station IDs
     %year: Four-digit year or array of years
     %month: One or two-digit month or array of months. Use 'all' to download a full year.
     %downloadedFilePath: designate path to download to. Type a 'folder
@@ -16,22 +15,27 @@
     %the working directory.
     %
     %Outputs:
-    %downloadedFilenames: Array of filenames created by the new files.
+    %downloadedFilenames: 3D cell array of filenames created by the new
+    %files. D1=year, D2=station, D3=month
     %
     %Note: if function is failing for no apparent cause, it is likely an NCDC server problem.
     %Wait some time and try again, or try a different email address.
     %
-    %Version Date: 2/21/2020
-    %Last major revision: 2/21/2020
+    %Version Date: 2/23/2020
+    %Last major revision: 2/23/2020
     %Written by: Daniel Hueholt
     %North Carolina State University
     %Undergraduate Research Assistant at Environment Analytics
     %
     
 function [downloadedFilenames] = ASOSdownloadFiveMin(emailAddress,station,year,month,downloadedFilePath)
-if nargin~=5 %Needs all inputs to work properly
+% Check inputs
+if nargin~=5
     msg = 'Improper number of inputs, check syntax!';
     error(msg);
+end
+if ischar(station)==1
+    station = {station};
 end
 
 % Check to see if whole year was requested
@@ -91,22 +95,26 @@ for y_c = length(year):-1:1
     %yearString = num2str(year);
     yearDirString{y_c} = strcat(yearPrefix{y_c},yearString{y_c}); %Creates the year directory string by concatenating the prefix and the input year
     yearPath{y_c} = strcat(fiveMinPath,yearDirString{y_c}); %This is the path for the input year
-    for m_c = length(month):-1:1
-        obsFilename{y_c,m_c} = [obsType zeroChar station yearString{y_c} monthString{m_c} fileExtension]; %If month is present, download the specific month file.
+    for st_c = length(station):-1:1
+        for m_c = length(month):-1:1
+            obsFilename{y_c,st_c,m_c} = [obsType zeroChar station{st_c} yearString{y_c} monthString{m_c} fileExtension]; %If month is present, download the specific month file.
+        end
     end
 end
 ftpNCDC = ftp('ftp.ncdc.noaa.gov','anonymous',emailAddress); %Opens an FTP connection to the NCDC server
 cd(ftpNCDC,fiveMinPath); %Changes folder to the ASOS five minute data
 for y_c = length(year):-1:1
     cd(ftpNCDC,yearPath{y_c}); %Changes folder to the year path
-    for m_c = length(month):-1:1
-        try
-            mget(ftpNCDC,obsFilename{y_c,m_c},downloadedFilePath); %Downloads target file(s) to the specified file path
-        catch
-            disp([obsFilename{y_c,m_c} ' could not be located on server! Skipped and continuing.'])
-            continue
+    for st_c = length(station):-1:1
+        for m_c = length(month):-1:1
+            try
+                mget(ftpNCDC,obsFilename{y_c,st_c,m_c},downloadedFilePath); %Downloads target file(s) to the specified file path
+            catch
+                disp([obsFilename{y_c,st_c,m_c} ' could not be located on server! Skipped and continuing.'])
+                continue
+            end
+            downloadedFilenames{y_c,st_c,m_c} = strcat(downloadedFilePath,obsFilename{y_c,st_c,m_c});
         end
-        downloadedFilenames{y_c,m_c} = strcat(downloadedFilePath,obsFilename{y_c,m_c});
     end
 end
 close(ftpNCDC) %Closes FTP connection
