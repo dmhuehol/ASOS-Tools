67c4c7de0721806b86f1665f33efe8d2069bac91
Daniel Hueholt <dmhuehol@ncsu.edu>
2020-06-16 20:56:36 -0400

    Locate start/end time and data
    Now locates start/end time and extracts data for storms in a given structure! Next: implement precipitation intensity

--- stormFinder.m
+++ stormFinder.m
@@ -1,34 +1,41 @@
 %%stormFinder
-%   General concept: give an ASOS seasonal structure
-%   return start time, end time, peak precipitation intensity for every
-%   storm, where 2 hour break defines storm end
+%   Given an structure of ASOS data, return start time, end time, and data
+%   for all storms in the season. Individual storms determined by 2-hour
+%   gaps.
+%
+%   General form: [storms] = stormFinder(ASOS)
+%
+%   Under development: Also return peak precipitation intensity for every
+%   storm
 %   2 hour break => 24 index gap
 %   Written by: Daniel Hueholt
 %   North Carolina State University
-%   Undergraduate Research Assistant at Environment Analytics
-%   Version Date: 6/8/2020
-%   Last Major Revision: 6/8/2020
+%   Research Assistant at Environment Analytics
+%   Version Date: 6/16/2020
+%   Last Major Revision: 6/16/2020
 %
 
 
-function [detectedStorms,detectedSnowstorms] = stormFinder(ASOS)
+function [storms] = stormFinder(ASOS)
 presentWeather = {ASOS.PresentWeather};
 
-%precipCode = {'RA','FZRA','SN','PL','DZ','FZDZ','SG'}; % all possible weather codes corresponding to precipitation
-precipCode = 'SN';
-weather = strfind(presentWeather,precipCode);
-noWeather = cellfun('isempty',weather); %'isempty' is faster than @isempty
-weather(noWeather) = {0}; %Insert 0 into all empty cells, otherwise conversion to double removes blank entries
-weather = cell2mat(weather); %Convert to double
-logicalWeather = logical(weather); %Logically index on ~0
-%All logical 1 elements now correspond to where the weather code is detected
-validInd = logicalWeather==1;
+precipCode = ["SN","BLSN","PL","DZ","FZDZ","RA","FZRA","SG","GS"]; % all possible weather codes corresponding to precipitation ordered usefully
+weather = contains(presentWeather,precipCode);
+% Where weather is 1, there is at least one precip code hit
+% where weather is 0, precip is not occurring
+[~,weatherInd,~] = find(weather); % Find indices where precip occurs
+weatherInd = weatherInd';
 
-indArr = 1:1:length(ASOS);
-precipInd = indArr(validInd);
+findGaps = diff(weatherInd); %Look for gaps in the indices where precip occurs
+gapLog = findGaps>24; %If the gap is larger than 24 indices, then it's longer than 2 hours
+[gapInd,~,~] = find(gapLog);
+weatherData = ASOS(weather);
 
+for wq = 1:length(gapInd)-1
+    storms(wq).data = weatherData(gapInd(wq)+1:gapInd(wq+1)-1); %#ok
+    storms(wq).startTime = storms(wq).data(1).Datetime; %#ok
+    storms(wq).endTime = storms(wq).data(end).Datetime; %#ok
+end
 
-detectedStorms = 'Cats';
-detectedSnowstorms = 'Rabbits';
 
 end
