d9372319b8917e7f1d6deb8d65b32199ae1c1f39
Daniel Hueholt <dmhuehol@ncsu.edu>
2020-06-16 23:22:25 -0400

    Implement intensity metric
    Scores precipitation intensity as described in the comments. Identifies the time of peak intensity and outputs relevant information.

--- stormFinder.m
+++ stormFinder.m
@@ -1,13 +1,28 @@
 %%stormFinder
-%   Given an structure of ASOS data, return start time, end time, and data
-%   for all storms in the season. Individual storms determined by 2-hour
-%   gaps.
+%   Given an structure of ASOS data, return start time, end time, hour of
+%   peak intensity, and data for all storms in the season. Individual
+%   storms determined by 2-hour gaps.
+%
+%   ASOS 5-minute data does not record snow liquid water, and snow liquid
+%   water is usually unreliable on this timescale anyway. We approximate
+%   peak intensity from the weather codes. All weather code entries are
+%   assigned a numerical intensity score (iScore): '+' = 3, none = 2, '-' =
+%   1. For storms of sufficient length, the event is scored by hour. The
+%   hours are then summed individually, and the hour with the highest
+%   iScore is considered the hour of peak intensity.
 %
 %   General form: [storms] = stormFinder(ASOS)
 %
-%   Under development: Also return peak precipitation intensity for every
-%   storm
-%   2 hour break => 24 index gap
+%   Input:
+%   ASOS: ASOS data structure, see ASOSimportFiveMin or ASOSimportManyFiveMin.
+%
+%   Output:
+%   storms: structure containing the storm information. The 'all'
+%   substructure contains every storm with precipitation codes from the
+%   input structure. The 'filtered' substructure requires that events have
+%   a total iScore above 15. This is an attempt to omit minimal-impact
+%   trace events.
+%
 %   Written by: Daniel Hueholt
 %   North Carolina State University
 %   Research Assistant at Environment Analytics
@@ -22,8 +37,7 @@ presentWeather = {ASOS.PresentWeather};
 precipCode = ["SN","BLSN","PL","DZ","FZDZ","RA","FZRA","SG","GS"]; % all possible weather codes corresponding to precipitation ordered usefully
 weather = contains(presentWeather,precipCode);
 % Where weather is 1, there is at least one precip code hit
-% where weather is 0, precip is not occurring
-% Instead of looping and removing codes, check for e.g. BLSN yes and SN no?
+% Where weather is 0, precip is not occurring
 [~,weatherInd,~] = find(weather); % Find indices where precip occurs
 weatherInd = weatherInd';
 
@@ -56,9 +70,30 @@ for wq = 1:length(gapInd)-1
         filterStorms(fc).endTime = allStorms(wq).endTime; %#ok
         filterStorms(fc).iScoreArr = allStorms(wq).iScoreArr; %#ok
         filterStorms(fc).iScore = iScoreScalar; %#ok
-        fc = fc+1;
-    end
         
+        stormDuration = filterStorms(fc).endTime-filterStorms(fc).startTime;
+        if stormDuration>hours(1)
+            activeDt = [allStorms(wq).data.Datetime]';
+            activeHour = activeDt.Hour;
+            allHours = unique(activeHour);
+            for hq = 1:length(allHours)
+                indHour = activeHour==allHours(hq);
+                hour(hq).datetime = activeDt(indHour);
+                hour(hq).iScoreArr = iScore(indHour);
+                hour(hq).iScore = sum(hour(hq).iScoreArr);
+            end
+            [maxScore,maxInd] = max([hour.iScore]);
+            peakIntensity.Hour = hour(maxInd).datetime(1).Hour;
+            peakIntensity.datetime = hour(maxInd).datetime;
+            peakIntensity.iScoreArr = hour(maxInd).iScoreArr;
+            peakIntensity.iScore = maxScore;
+            filterStorms(fc).peak = peakIntensity; %#ok
+            filterStorms(fc).peakHourStart = peakIntensity.datetime(1);
+        end
+        fc = fc+1;
+        
+    end
+    
 end
 
 % Make final output structure
