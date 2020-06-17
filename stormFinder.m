8e0eaa486a3c926fb4b757d03babc40584d74fc7
Daniel Hueholt <dmhuehol@ncsu.edu>
2020-06-16 21:28:57 -0400

    Score precip codes
    We approximate precip intensity by scoring the precipitation codes. Storms with a very low intensity score are filtered out, loosely analogous to filtering by a "trace" of precipitation.

--- stormFinder.m
+++ stormFinder.m
@@ -23,6 +23,7 @@ precipCode = ["SN","BLSN","PL","DZ","FZDZ","RA","FZRA","SG","GS"]; % all possibl
 weather = contains(presentWeather,precipCode);
 % Where weather is 1, there is at least one precip code hit
 % where weather is 0, precip is not occurring
+% Instead of looping and removing codes, check for e.g. BLSN yes and SN no?
 [~,weatherInd,~] = find(weather); % Find indices where precip occurs
 weatherInd = weatherInd';
 
@@ -31,11 +32,37 @@ gapLog = findGaps>24; %If the gap is larger than 24 indices, then it's longer th
 [gapInd,~,~] = find(gapLog);
 weatherData = ASOS(weather);
 
+codesOnly = {weatherData.PresentWeather};
+heavy = contains(codesOnly,'+');
+light = contains(codesOnly,'-');
+[~,heavyInd,~] = find(heavy);
+[~,lightInd,~] = find(light);
+iScore = ones(length(codesOnly),1)*2;
+iScore(heavyInd) = 3;
+iScore(lightInd) = 1;
+
+fc = 1;
 for wq = 1:length(gapInd)-1
-    storms(wq).data = weatherData(gapInd(wq)+1:gapInd(wq+1)-1); %#ok
-    storms(wq).startTime = storms(wq).data(1).Datetime; %#ok
-    storms(wq).endTime = storms(wq).data(end).Datetime; %#ok
+    allStorms(wq).data = weatherData(gapInd(wq)+1:gapInd(wq+1)-1); %#ok
+    allStorms(wq).startTime = allStorms(wq).data(1).Datetime; %#ok
+    allStorms(wq).endTime = allStorms(wq).data(end).Datetime; %#ok
+    allStorms(wq).iScoreArr = iScore(gapInd(wq)+1:gapInd(wq+1)-1); %#ok
+    iScoreScalar = sum(allStorms(wq).iScoreArr);
+    allStorms(wq).iScore = iScoreScalar; %#ok
+    
+    if iScoreScalar > 15
+        filterStorms(fc).data = allStorms(wq).data; %#ok
+        filterStorms(fc).startTime = allStorms(wq).startTime; %#ok
+        filterStorms(fc).endTime = allStorms(wq).endTime; %#ok
+        filterStorms(fc).iScoreArr = allStorms(wq).iScoreArr; %#ok
+        filterStorms(fc).iScore = iScoreScalar; %#ok
+        fc = fc+1;
+    end
+        
 end
 
+% Make final output structure
+storms.all = allStorms;
+storms.filtered = filterStorms;
 
 end
